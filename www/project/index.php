45e527f087ec6cbd1890b4b6a6ff320c39936626
haleyandres <haleyeandres@gmail.com>
2025-04-25 20:09:49 -0400

    link to published site

--- www/project/index.php
+++ www/project/index.php
@@ -1,12 +1,12 @@
 <?php
-// published site: 
+// published site: https://cs4640.cs.virginia.edu/vwz2wb/project/
 
 // DEBUGGING ONLY! Show all errors.
 error_reporting(E_ALL);
 ini_set("display_errors", 1);
 
 spl_autoload_register(function ($classname) {
-        // include "/students/vwz2wb/students/vwz2wb/private/hw6/$classname.php";
+        // include "/students/vwz2wb/students/vwz2wb/private/project/$classname.php";
         include "/opt/src/project/$classname.php";
 });
 
